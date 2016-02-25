f34a81043e0cb6c4bb1b12546ba13ad37e5a96c4
brac45 <texas45@naver.com>
2016-02-25 11:33:34 +0900

    Initial commit

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Assets.Forms;
+using System.Collections;
+
+public class SceneManagerScript : MonoBehaviour {
+    private Form1 form;
+
+	// Use this for initialization
+	void Start () {
+        ShowWindowsForm();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Show empty windows form
+    void ShowWindowsForm () {
+        form = new Form1();
+        form.Show();
+    }
+}
